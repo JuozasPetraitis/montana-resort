891f9178833cf6dd1135516d770c0f7fc393a2bc
JoeAliGit <82863157+JoeAliGit@users.noreply.github.com>
2021-12-01 22:10:19 +0200

    No.1: Initializing starter for development

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+//! Pages
+import HomePage from './pages/HomePage';
+import RoomsPage from './pages/RoomsPage';
+import AboutPage from './pages/AboutPage';
+
+//! Main Component
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="rooms" element={<RoomsPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
