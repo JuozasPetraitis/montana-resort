b9dcbe4c319bacb10b0c405deddc8a0f729f15ae
JoeAliGit <82863157+JoeAliGit@users.noreply.github.com>
2021-12-02 17:58:00 +0200

    No5: Mini fix.

--- frontend/src/components/molecules/AboutUsSection.jsx
+++ frontend/src/components/molecules/AboutUsSection.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 //! CSS
 //! IMG
-import BigPool from '../../assets/images/4.jpg';
-import BigPoolArea from '../../assets/images/3.jpg';
+import HotelArea from '../../assets/images/Area.jpg';
+import PoolArea from '../../assets/images/PoolArea.jpg';
 
 //! Main Component
 const AboutUsSection = () => {
@@ -26,10 +26,10 @@ const AboutUsSection = () => {
 
         <div className="flex m-auto lg:w-3/5 ml-5">
           <div className="w-1/2 h-60 pb-8 mr-2 md:h-80 lg:h-500px">
-            <img className="w-full h-full object-cover" src={BigPool} alt="Pool" />
+            <img className="w-full h-full object-cover" src={HotelArea} alt="Pool" />
           </div>
           <div className="w-1/2 h-60 pt-8 md:h-80 lg:ml-5">
-            <img className="w-full h-full object-cover" src={BigPoolArea} alt="Pool Chairs" />
+            <img className="w-full h-full object-cover" src={PoolArea} alt="Pool Chairs" />
           </div>
         </div>
       </div>
