03130d2231fb2be077888d52852ffdc5fdacc32c
JoeAliGit <82863157+JoeAliGit@users.noreply.github.com>
2021-12-09 15:59:44 +0200

    Mini fix

---
+++ frontend/src/components/molecules/AboutUsDescription.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AboutUsDescription = () => {
+  return (
+    <div className="container m-auto flex flex-col md:flex-row gap-4 px-1 md:px-3 lg:w-3/4">
+      <div className="max-w-prose">
+        <p className="text-xl font-normal text-center tracking-tighter mb-3">We Serve Fresh and Delicious Food</p>
+        <p className="text-sm tracking-wide font-light">
+          Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus enim ut
+          tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. Dapibus lorem
+          feugiat facilisi faucibus et. Rhoncus.
+        </p>
+      </div>
+
+      <div className="max-w-prose">
+        <p className="text-xl font-normal text-center tracking-tighter mb-3">We Serve Fresh and Delicious Food</p>
+        <p className="text-sm tracking-wide font-light">
+          Suscipit libero pretium nullam potenti. Interdum, blandit phasellus consectetuer dolor ornare dapibus enim ut
+          tincidunt rhoncus tellus sollicitudin pede nam maecenas, dolor sem. Neque sollicitudin enim. Dapibus lorem
+          feugiat facilisi faucibus et. Rhoncus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsDescription;
